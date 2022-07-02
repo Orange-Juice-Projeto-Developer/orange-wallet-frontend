4ddc4192f66dc39ec90fe5b6ea7e2f3bcefb9683
Matheus Zarpellon <mvzarpellon@gmail.com>
2022-07-01 22:27:37 -0300

    :wrench: Configuração da estrutura inicial do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+			Hello World
+			<button>OK</button>
+		</div>
+	);
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
+module.exports = {
+	content: ["./src/**/*.tsx"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
