16bf386e3c2a50cdbd1dc745b2be75365be94bf3
matheusgondra <matheusgondra951@gmail.com>
2022-07-06 23:08:45 -0300

    :sparkles: feat: Criação do componente de cabeçalho

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import imagePerfil from "../../assets/images/perfil.png";
+import sinoIcon from "../../assets/images/sino.svg";
+import eyeIcon from "../../assets/images/bi_eye-slash.svg";
+import dolarIcon from "../../assets/images/dolar.svg";
+import plusIcon from "../../assets/images/icon-button-plus.svg";
+
+function Header() {
+	return (
+		<header className="bg-gradient-to-b from-[rgba(19,19,19,0.4126)] to-[#323131] rounded-b-[30px] pb-[26px] font-roboto">
+			<div className="flex justify-between px-4 pt-[26px]">
+				<div>
+					<span className="text-base text-white">Ola,</span>
+					<br/>
+					<span className="text-2xl text-white">Theodoro</span>
+				</div>
+				<div>
+					<img className="inline mr-4" src={sinoIcon} alt="Ícone de notificações" />
+					<img className="inline" src={imagePerfil} alt="Foto de perfil" />
+				</div>
+			</div>
+			<div className="bg-gray rounded-lg mx-3 my-7 px-2 py-4 flex justify-between items-end">
+				<div>
+					<span className="block text-white text-sm mb-3">Seu saldo atual é de</span>
+					<span className="text-2xl text-white">R$ 8.050,00</span>
+					<img className="inline ml-4 mb-[5px]" src={eyeIcon} alt="Ícone de olho cortado" />
+				</div>
+				<img src={dolarIcon} alt="Ícone de dolar" />
+			</div>
+			<button className="text-black text-base font-bold bg-orange rounded-3xl py-[14px] px-24 mx-auto flex items-center">
+				<img className="inline mr-2" src={plusIcon} alt="" />
+				Cadastrar transação
+			</button>
+		</header>
+	);
+}
+
+export default Header;
