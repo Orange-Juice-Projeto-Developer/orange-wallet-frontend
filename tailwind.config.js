ab7496fa8f27a636437ed8eb0f511edf53be83ee
Matheus Zarpellon <mvzarpellon@gmail.com>
2022-07-08 02:36:49 -0300

    :sparkles: Criação do componente de cabeçalho

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
 	content: ["./src/**/*.tsx"],
 	theme: {
 		extend: {},
+		colors: {
+			white: "#F8F9FA",
+			black: "#151515",
+			gray: "#383838",
+			orange: "#FF8A00",
+			background: "#131313"
+		},
+		fontFamily: {
+			roboto: ["Roboto", "sans-serif"]
+		}
 	},
 	plugins: [],
 };
